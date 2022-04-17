2aa58b18d2ef997627ea8d3e58269d9a0026565e
B324W17 <bezawit.teklemariam@gmail.com>
2022-04-17 21:37:19 +0200

    create additional pages

---
+++ src/drive-frontend/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app id="app">
+    <my-appbar />
+    <my-body-section />
+  </v-app>
+</template>
+
+<script>
+import MyAppbar from "@/components/public/myAppbar.vue";
+
+import MyBodySection from "@/components/public/myBodySection.vue";
+
+export default {
+  name: "Home-page",
+
+  components: {
+    MyAppbar,
+    MyBodySection,
+  },
+};
+</script>
