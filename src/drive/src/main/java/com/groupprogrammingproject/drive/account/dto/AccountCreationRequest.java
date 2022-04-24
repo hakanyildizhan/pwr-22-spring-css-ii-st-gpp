12ebcc9d1bd85c99e9360d7af146c81d592f106a
uminskiM <marcinuminski1998@gmail.com>
2022-04-24 19:07:40 +0200

    Implement strong password rules checks:

--- src/drive/src/main/java/com/groupprogrammingproject/drive/account/dto/AccountCreationRequest.java
+++ src/drive/src/main/java/com/groupprogrammingproject/drive/account/dto/AccountCreationRequest.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import static com.groupprogrammingproject.drive.exception.ExceptionMessage.CANNOT_BE_BLANK_MESSAGE;
 import static com.groupprogrammingproject.drive.exception.ExceptionMessage.MUST_MATCH_EMAIL_PATTERN_MESSAGE;
@@ -21,6 +23,8 @@ public class AccountCreationRequest {
     @NotBlank(message = "Email" + CANNOT_BE_BLANK_MESSAGE)
     private final String email;
 
+    @Length(min = 8)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$")
     @NotBlank(message = "Password" + CANNOT_BE_BLANK_MESSAGE)
     private final String password;
 
