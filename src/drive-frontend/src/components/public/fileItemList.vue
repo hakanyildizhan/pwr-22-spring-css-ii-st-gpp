fc08b9b7fa3494ccf524fa4a8b037689c1f1297b
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-06-16 22:26:18 +0200

    Initial file listing, context menu to create folders

--- src/drive-frontend/src/components/public/fileItemList.vue
+++ src/drive-frontend/src/components/public/fileItemList.vue
@@ -1,135 +1,6 @@
-<script>
-import fileItem from "./fileItem";
-
-export default {
-  name: "fileItemList",
-  components: {
-    fileItem,
-  },
-  data() {
-    return {
-      files:[
-        {
-          name:'cv.pdf',
-          owner:'me',
-          lastmodified:'2022-02-01',
-          filesize:1.2
-        },
-         {
-          name:'ml_for_researcher.pdf',
-          owner:'me',
-          lastmodified:'2022-02-01',
-          filesize:1.2
-        }
-      ],
-      items: [
-        {
-          id: "0B0tQEjbz9YbuS1ZQdXdaSGRydzg",
-          type: "folder",
-          title: "Folder 1",
-        },
-        {
-          id: "0B0tQEjbz9YbuS1ZAdXdaSGRydzg",
-          type: "folder",
-          title: "Folder 2",
-        },
-        {
-          id: "0B0tQEjbz9YbuS1ZAdWdaaGRydzg",
-          type: "folder",
-          title: "Folder 3",
-        },
-        {
-          id: "0B0tQajbz9YbuS1ZAdXdaaGRydzg",
-          type: "file",
-          title: "File 1",
-        },
-        {
-          id: "0B0tQajbz9YbuS1ZAdXdaaGRadzg",
-          type: "file",
-          title: "File 2",
-        },
-      ],
-    };
-  },
-  computed:{
-  //   sortedfiles(){
-  //     return this.files.sort((a,b)=>{
-  //         const aName  = a.name.toUpperCase();
-  //         const bName  = b.name.toUpperCase();
-  //         let comparison = 0;
-  //         if (aName > bName) {
-  //   comparison = 1;
-  // } else if (aName < bName) {
-  //   comparison = -1;
-  // }
-  //         return comparison;
-  //     });
-  //   }
-   
-  },
-  methods: {
-    getFolders() {
-      return this.items.filter((item) => item.type == "folder");
-    },
-    getFiles() {
-      return this.items.filter((item) => item.type == "file");
-    },
-    startDrag(event, item) {
-      event.dataTransfer.dropEffect = "move";
-      event.dataTransfer.effectAllowed = "move";
-      event.dataTransfer.setData("itemID", item.id);
-    },
-    onDrop(event) {
-      const sourceItemId = event.dataTransfer.getData("itemID");
-      const sourceItem = this.items.filter((i) => i.id == sourceItemId)[0];
-      const targetItem = this.items.filter(
-        (i) => i.title == event.target.outerText.trim()
-      )[0];
-      if (sourceItem.id == targetItem.id || targetItem.type == "file") {
-        return;
-      }
-      console.log(sourceItem.title + "-->" + targetItem.title);
-    },
-  },
-};
-</script>
-
 <template>
-  <v-main>
+  <v-main v-contextmenu:contextmenu>
     <v-container fluid class="grey lighten-5 mb-6">
-      <!-- start data table -->
-         <v-table>
-           <thead>
-             <th class="text-left">
-                Name
-                <v-icon
-      large
-      color="green darken-2"
-    >
-      mdi-arrow-up
-    </v-icon>
-             </th>
-             <th class="text-left">
-                Owner
-             </th>
-             <th class="text-left">
-                Last Modified
-             </th>
-              <th class="text-left">
-                File Size
-             </th>
-           </thead>
-             <tbody>
-               <tr v-for="(file,index) in files" :key="index">
-               <td>  {{file.name}}</td>
-               <td>  {{file.owner}}</td>
-               <td>  {{file.lastmodified}}</td>
-               <td>  {{file.filesize}}</td>
-                
-               </tr>
-             </tbody>
-         </v-table>
-
       <!-- end data table  -->
       <div class="item-type">Folders</div>
       <div
@@ -177,59 +48,160 @@ export default {
 
     </v-container>
 
-    <v-btn class="upload"
-          icon="mdi-plus" 
-          fab dark absolute 
-          color="indigo"
-          @click="openFilePicker"/>
+    <v-btn
+      class="upload"
+      icon="mdi-plus"
+      fab
+      dark
+      absolute
+      color="indigo"
+      @click="openFilePicker"
+    />
 
-    <v-file-input class="file-input"
-                  @change="uploadFile">
-    </v-file-input>
+    <v-file-input class="file-input" @change="uploadFile"> </v-file-input>
   </v-main>
+
+  <v-contextmenu ref="contextmenu">
+    <v-contextmenu-item @click="openCreateFolderPopup">Create Folder</v-contextmenu-item>
+  </v-contextmenu>
+
+  <div class="text-center">
+    <v-dialog
+      v-model="createFolderPopupIsVisible"
+    >
+      <v-card width="300" style="padding:20px;">
+
+          <v-text-field
+            v-model="folderToCreate"
+            label="Folder Name"
+            variant="outlined"
+            shaped clearable
+          ></v-text-field>
+
+        <v-card-actions>
+          <v-row>
+            <v-col sm="6" align="center" justify="center">
+              <v-btn color="primary" @click="createFolderPopupIsVisible = false; this.folderToCreate = 'New folder'">Cancel</v-btn>
+            </v-col>
+            <v-col sm="6" align="center" justify="center">
+              <v-btn color="primary" @click="createFolder">Create</v-btn>
+            </v-col>
+          </v-row>
+          
+          
+        </v-card-actions>
+
+      </v-card>
+    </v-dialog>
+  </div>
+
 </template>
 
 <script>
 import fileItem from "./fileItem";
-//const http = require('http');
-//import * as http from 'http'
+import { store } from "../../store";
+import { directive, Contextmenu, ContextmenuItem } from "v-contextmenu";
+import "v-contextmenu/dist/themes/default.css";
 
 export default {
   name: "fileItemList",
   components: {
     fileItem,
+    [Contextmenu.name]: Contextmenu,
+    [ContextmenuItem.name]: ContextmenuItem,
   },
   data() {
     return {
-      id: 0,
-      items: [
-        // {
-        //   id: "0B0tQEjbz9YbuS1ZQdXdaSGRydzg",
-        //   type: "folder",
-        //   title: "Folder 1",
-        // },
-        // {
-        //   id: "0B0tQEjbz9YbuS1ZAdXdaSGRydzg",
-        //   type: "folder",
-        //   title: "Folder 2",
-        // },
-        // {
-        //   id: "0B0tQEjbz9YbuS1ZAdWdaaGRydzg",
-        //   type: "folder",
-        //   title: "Folder 3",
-        // },
-        // {
-        //   id: "0B0tQajbz9YbuS1ZAdXdaaGRydzg",
-        //   type: "file",
-        //   title: "File 1",
-        // },
-        // {
-        //   id: "0B0tQajbz9YbuS1ZAdXdaaGRadzg",
-        //   type: "file",
-        //   title: "File 2",
-        // },
+      folderToCreate: "New folder",
+      createFolderPopupIsVisible: false,
+      showMenu: false,
+      x: 0,
+      y: 0,
+      menuItems: [
+        { title: "style 1 title", menuAction: "style 1" },
+        {
+          title: "style 2 title",
+          menuAction: (action) => {
+            alert(action);
+          },
+        },
+        {
+          title: "style 3 title",
+          menuAction: (action) => {
+            this.someFunc(action);
+          },
+        },
       ],
+      currentPath: ".",
+      loading: false,
+      showErrorSnack: false,
+      store,
+      items: [],
     };
+  },
+  mounted() {
+    // fetch user's files on init
+    this.loading = true;
+
+    const formData = new FormData();
+    formData.append("parentFolder", ".");
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + this.store.getToken(),
+      },
+      body: formData,
+    };
+    fetch(process.env.VUE_APP_BACKEND_URL + "/files/list", requestOptions)
+      .then((response) => {
+        this.loading = false;
+
+        if (response.ok) {
+          response.json().then((returnedItems) => {
+            this.items = [];
+            returnedItems.forEach((item) =>
+              this.items.push({
+                id: item.id,
+                type: item.type == 2 ? "file" : "folder",
+                title: item.displayName,
+              })
+            );
+          });
+        } else {
+          response.text().then((err) => {
+            let error = "An unknown error occurred. Please try again.";
+            this.showErrorSnack = true;
+            if (err == "Token expired") {
+              this.store.logout();
+              this.$router.push({ name: "login" });
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        let error = "An unknown error occurred. Please try again.";
+        this.showErrorSnack = true;
+        this.loading = false;
+      });
+  },
+  directives: {
+    contextmenu: directive,
+  },
+  computed: {
+    //   sortedfiles(){
+    //     return this.files.sort((a,b)=>{
+    //         const aName  = a.name.toUpperCase();
+    //         const bName  = b.name.toUpperCase();
+    //         let comparison = 0;
+    //         if (aName > bName) {
+    //   comparison = 1;
+    // } else if (aName < bName) {
+    //   comparison = -1;
+    // }
+    //         return comparison;
+    //     });
+    //   }
   },
   methods: {
     getFolders() {
@@ -255,7 +227,10 @@ export default {
       console.log(sourceItem.title + "-->" + targetItem.title);
     },
     openFilePicker() {
-      document.getElementsByClassName('file-input')[0].querySelector('input').click();
+      document
+        .getElementsByClassName("file-input")[0]
+        .querySelector("input")
+        .click();
     },
     uploadFile(e) {
       var files = e.target.files;
@@ -263,85 +238,130 @@ export default {
         return;
       }
 
-      var file = files[0]
-      console.log(file)
+      var file = files[0];
+      console.log(file);
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("parentFolder", this.currentPath);
 
-      var FormData = require('form-data')
-      var form = new FormData();
-      form.append('file', file);
-      form.append('filename', file.name);
-
-      // var options = {
-      //   method: 'post',
-      //   host: 'http://localhost:8080',
-      //   path: '/files/uploadFile',
-      // }
-
-      
-      // const request = http.request({
-      //   method: 'post',
-      //   host: 'http://localhost:8080',
-      //   path: '/files/uploadFile',
-      // })
-
-      // form.pipe(request);
-
-      // request.on('response', function(res) {
-      //   console.log(res.statusCode);
-      // });
-
-      // form.submit(options, function(err, res) {
-      //   console.log(res.statusCode);
-      // })
-
-      this.items.push({
-          id: ++this.id,
-          type: "file",
-          title: file.name,
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + this.store.getToken(),
+        },
+        body: formData,
+      };
+      fetch(process.env.VUE_APP_BACKEND_URL + "/files/upload", requestOptions)
+        .then((response) => {
+          this.loading = false;
+          if (!response.ok) {
+            // show error
+            let error = "An unknown error occurred. Please try again.";
+            this.showErrorSnack = true;
+          } else {
+            return response;
+          }
         })
+        .then((itemId) => {
+          this.items.push({
+            id: itemId,
+            type: "file",
+            title: file.name,
+          });
+        })
+        .catch((err) => {
+          let error = "An unknown error occurred. Please try again.";
+          this.showErrorSnack = true;
+          this.loading = false;
+        });
+    },
+    openCreateFolderPopup() {
+      this.createFolderPopupIsVisible = true;
+    },
+    createFolder() {
+      this.createFolderPopupIsVisible = false;
+      let folder = this.folderToCreate;
+      this.folderToCreate = 'New folder';
+      if (!folder) {
+        return;
+      }
+      
+      this.loading = true;
+      const formData = new FormData();
+      formData.append("folderName", folder);
+      formData.append("parentFolder", this.currentPath);
+      
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + this.store.getToken(),
+        },
+        body: formData,
+      };
+      fetch(process.env.VUE_APP_BACKEND_URL + "/files/createFolder", requestOptions)
+        .then((response) => {
+          this.loading = false;
+          if (!response.ok) {
+            // show error
+            throw("Server responded with " + response.status);
+          } else {
+            return response;
+          }
+        })
+        .then((itemId) => {
+          this.items.push({
+            id: itemId,
+            type: "folder",
+            title: folder,
+          });
+        })
+        .catch((err) => {
+          this.showErrorSnack = true;
+          this.loading = false;
+        });
     }
   },
 };
 </script>
 
 <style>
-  .item-list {
-    grid-area: view;
-    overflow: hidden;
-    position: relative;
-  }
+.item-list {
+  grid-area: view;
+  overflow: hidden;
+  position: relative;
+}
 
-  .item-list-wrapper {
-    flex: 1 1 auto;
-    height: 100%;
-    width: 100%;
-    overflow-y: scroll;
-    position: relative;
-    display: flex;
-    -webkit-flex-flow: column nowrap;
-    flex-flow: column nowrap;
-  }
+.item-list-wrapper {
+  flex: 1 1 auto;
+  height: 100%;
+  width: 100%;
+  overflow-y: scroll;
+  position: relative;
+  display: flex;
+  -webkit-flex-flow: column nowrap;
+  flex-flow: column nowrap;
+}
 
-  .item-type {
-    color: #5f6368;
-    margin-left: 22px;
-    padding: 8px 0 16px;
-    display: block;
-  }
+.item-type {
+  color: #5f6368;
+  margin-left: 22px;
+  padding: 8px 0 16px;
+  display: block;
+}
 
-  .item-grid {
-    display: grid;
-    grid-gap: 0;
-    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-  }
+.item-grid {
+  display: grid;
+  grid-gap: 0;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+}
 
-  .upload {
-    position: absolute !important; 
-    bottom: 10% !important; 
-    right: 25% !important;
-  }
+.upload {
+  position: absolute !important;
+  bottom: 10% !important;
+  right: 25% !important;
+}
 
-  .file-input {
-    display: none;
-  }
+.file-input {
+  display: none;
+}
 </style>
