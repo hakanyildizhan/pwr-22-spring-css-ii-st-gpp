3e2bbc24ed23cb5161c4373f31495f76324749ca
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-06-19 12:33:55 +0200

    Fix for file downloads

--- src/drive/src/main/java/com/groupprogrammingproject/drive/files/download/FileDownloadService.java
+++ src/drive/src/main/java/com/groupprogrammingproject/drive/files/download/FileDownloadService.java
@@ -1,7 +1,11 @@
 package com.groupprogrammingproject.drive.files.download;
 
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.groupprogrammingproject.drive.domain.file.Item;
+import com.groupprogrammingproject.drive.domain.file.ItemRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,11 +22,28 @@ import java.io.IOException;
 public class FileDownloadService {
 
     private final AmazonS3 amazonS3;
+    private final ItemRepository itemRepository;
 
     @Value("${amazon.s3.bucket}")
     private String bucketName;
 
     public Resource downloadFile(String key) throws IOException {
-        return new ByteArrayResource(amazonS3.getObject(bucketName, key).getObjectContent().readAllBytes());
+        Item item = itemRepository.findById(key).orElse(null);
+        if (item == null) {
+            log.error("Item with key {} was not found in the repository", key);
+            return null;
+        }
+        String path = item.getPath();
+
+        try {
+            
+            return new ByteArrayResource(amazonS3.getObject(bucketName, path).getObjectContent().readAllBytes());
+        } catch (AmazonServiceException e) {
+            log.error("S3 Object with key path {} was not found in the bucket: {}", path, e.getErrorMessage());
+            return null;
+        } catch (IOException e) {
+            log.error("Error while reading bytes from S3 Object with key path {}: {}", path, e.getMessage());
+            return null;
+        }
     }
 }
